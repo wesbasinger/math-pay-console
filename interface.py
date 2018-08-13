911d71922823d7b86c2d3810eaef8fd657d36b82
Wes Basinger <wbasinger@villagetechschools.org>
2018-08-13 12:27:37 -0500

    Able to make payments from a spreadsheet.

--- interface.py
+++ interface.py
@@ -1,6 +1,8 @@
 from json import load
 from block_io import BlockIo
 
+import csv
+
 version = 2
 
 with open('secret.json') as f:
@@ -13,4 +15,66 @@ block_io = BlockIo(
     version
 )
 
-print(block_io.get_my_addresses())
+def create_new_label(label):
+
+    result = block_io.get_new_address(label=label)
+
+    if(result['status'] == 'success'):
+
+        print(f'Address for {label} created successfully.')
+
+    else:
+
+        print("Error occurred while creating address.")
+
+def deposit(label, amount):
+
+    try:
+
+        result = block_io.withdraw_from_labels(
+            amounts=amount,
+            from_labels="default",
+            to_labels=label
+        )
+
+    except:
+
+        print(f'Address for {label}  does not exist, creating one now.')
+
+        create_new_label(label)
+
+        result = block_io.withdraw_from_labels(
+            amounts=amount,
+            from_labels="default",
+            to_labels=label
+        )
+
+    if(result['status'] == 'success'):
+
+        print(f'Amount of {amount} successfully sent to {label}.')
+
+    else:
+
+        print("Error occured while sending amount.")
+
+def pay_ledger():
+
+    with open('ledger.csv') as csvfile:
+
+        reader = csv.DictReader(csvfile)
+
+        if("Email" not in reader.fieldnames or "Payment" not in reader.fieldnames):
+
+            print("File error!  Check headers.")
+
+        else:
+
+            for row in reader:
+
+                if (int(row['Payment']) < 2):
+
+                    print("Skipping deposit, amount is less than 2.")
+
+                else:
+
+                    deposit(row['Email'], row['Payment'])
--- main.py
+++ main.py
@@ -1,13 +1,44 @@
+from sys import exit
 
+import interface
+
+def print_menu():
+
+    print("1.  Make Deposit to Single Account")
+    print("2.  Make Payments from Ledger")
+    print("0. Exit")
 
 def main():
 
     while True:
 
         print("Welcome to the console client for MathPay")
-        print("Please select from the following menu options")
+        print_menu()
 
-        input()
+        try:
+
+            int_input = int(input("Please select from the following menu options:   "))
+
+        except:
+
+            print("Please use numerical entries")
+
+        if(int_input == 0):
+
+            print("Goodbye")
+            exit()
+
+        elif(int_input == 1):
+
+            label = input("What is the email for the account you wish to deposit to?   ")
+
+            amount = int(input("How much would you like to deposit?   "))
+
+            interface.deposit(label, amount)
+
+        elif(int_input == 2):
+
+            interface.pay_ledger()
 
 if __name__ == '__main__':
     main()
